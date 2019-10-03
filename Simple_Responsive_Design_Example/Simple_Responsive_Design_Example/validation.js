f683f9a5f1668f9eee77b8831ac44dc45e2fb184
Patrik Racz <55232663+Patrik0161@users.noreply.github.com>
2019-10-03 16:12:20 +0100

    design
    1111111

---
+++ Simple_Responsive_Design_Example/Simple_Responsive_Design_Example/validation.js
@@ -0,0 +1,39 @@
+function validate() {
+  if( document.myForm.Name.value == "" )
+  {
+    alert( "Please provide your name!" );
+    document.myForm.Name.focus() ;
+    return false;
+  }
+
+  if( document.myForm.EMail.value == "" )
+  {
+    alert( "Please provide your Email!" );
+    document.myForm.EMail.focus();
+    return false;
+  }
+
+  var emailID = document.myForm.EMail.value
+  atpos = emailID.indecOf("@");
+  dotpos = emailID.lastIndexOf(".");
+
+  if (atpos < 1 || ( dotpos - atpos < 2)) {
+    alert("Please enter correct email ID!")
+    document.myForm.EMail.focus() ;
+    return false;
+  }
+
+  if( document.myForm.PostCode.value == "")
+  {
+    aler( "Please provide a postcode in the format ### ###.");
+    document.myForm.Postcode.focus() ;
+    return false;
+  }
+
+  if( document.myForm.Choice.value == "-1" )
+  {
+    alert( "Please select your choice!" );
+    return false;
+  }
+  return( true );
+}
